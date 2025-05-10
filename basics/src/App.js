876b385f76c164987e8ae6fb4e3365297939693a
vkama <krishna.amandu@gmail.com>
2025-05-10 15:21:34 +0530

    jsx example

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 function App() {
-  return (
-    <div className="App">
-     Learn React
-    </div>
-  );
+  return <h1>Hello, JSX!</h1>;
 }
+
+// Render the component inside the root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 export default App;
